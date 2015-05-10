b1ef576dce450392abd591f481ee09d9c4d251a0
Justin Watt <justinwatt@gmail.com>
2015-05-10 14:47:45 -0700

    Add delete specs and function

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,16 +1,16 @@
 class Node
 
-  attr_reader :data
-  attr_accessor :next
+  attr_accessor :next, :data
 
-  def initialize(data=nil)
+  def initialize(data = nil)
     @next = nil
     @data = data
   end
-
 end
 
 class LinkedList
+  include Enumerable
+
   attr_accessor :head, :tail
 
   def initialize
@@ -20,18 +20,45 @@ class LinkedList
   end
 
   def node_count
-    count = 0
-    traverse = head
-    until traverse.next == tail
-      count += 1
+    self.count
+  end
+
+  def delete(target)
+
+    false if empty?
+
+    traverse = head.next
+    until traverse == tail
+      if traverse.data == target
+        traverse.data = traverse.next.data
+        traverse.next = traverse.next.next
+        return target
+      else
+        traverse = traverse.next
+      end
+    end
+    false
+  end
+
+  def each &block
+    traverse = head.next
+    until traverse == tail
+      yield traverse if block_given?
       traverse = traverse.next
     end
-    count
+  end
+
+  def empty?
+    head.next == tail
   end
 
   def insert_to_front(data)
     new_node = Node.new(data)
     new_node.next = head.next
     head.next = new_node
+  end
+
+  def inspect
+    collect { |x| x.data }
   end
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -4,28 +4,54 @@ describe 'linked list' do
 
   let(:list) { LinkedList.new }
 
-  context 'when the list has no nodes' do 
+  context 'when a list has no nodes' do
     it 'can be created without error' do
       expect { list }.not_to raise_error
     end
 
     it 'has no nodes' do
-      expect(list.node_count).to be == 0 
+      expect(list.empty?).to be_truthy
     end
 
     it 'a node can be inserted to the front' do
       list.insert_to_front(5)
       expect(list.node_count).to be == 1
+      expect(list.inspect).to be == [5]
     end
 
     it 'several nodes can be inserted' do
-      (1..5).each do |x|
+      5.downto(1).each do |x|
         list.insert_to_front(x)
       end
       expect(list.node_count).to be == 5
+      expect(list.inspect).to be == [1, 2, 3, 4, 5]
     end
   end
-  
-end
 
-  
+  describe '#delete' do
+    describe 'empty list' do
+      it 'won\'t delete empty list' do
+        expect(list.delete(5)).to be_falsey
+      end
+    end
+
+    describe 'list with data' do
+      before(:example) do
+        5.downto(1).each do |x|
+          list.insert_to_front(x)
+        end
+      end
+
+      it 'target found' do
+        expect(list.delete(3)).to be == 3
+        expect(list.inspect).to be == [1, 2, 4, 5]
+      end
+
+      it 'can\'t find target' do
+        expect(list.delete(0)).to be_falsey
+        expect(list.inspect).to be == [1, 2, 3, 4, 5]
+      end
+    end
+  end
+
+end
